605d81c4c4a0a75284bae11d8ac13b46786aaf5a
Omer Hanan <omer.hanan@gmail.com>
2022-05-16 00:11:28 +0300

    Add multithreading functions for number of given threads

--- PrimeCalc/Program.cs
+++ PrimeCalc/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace PrimeCalc
 {
@@ -6,7 +7,28 @@ namespace PrimeCalc
     {
         static void Main(string[] args)
         {
-            
+            int lowerbound = int.Parse(args[0]);
+            int upperbound = int.Parse(args[1]);
+            int numOfThreads = int.Parse(args[2]);
+            Thread[] threads = new Thread[numOfThreads];
+            for (int i = 0; i < numOfThreads; i++) 
+            {
+                int range = (int) (upperbound-lowerbound) / numOfThreads;
+                int startAt = i * range;
+                int endAt = (i+1) * range;
+
+                Thread thread = new Thread(() => 
+                    DoWork(startAt, endAt)); // callBack
+                thread.Start();
+                thread.Join();
+            }
+        }
+
+        public static void DoWork(int lowerBound, int upperBound) 
+        {
+            for (int i = lowerBound; i < upperBound; i++)
+                if (isPrime(i)) 
+                    Console.WriteLine(i);
         }
 
         public static Boolean isPrime(int number) 
@@ -15,9 +37,7 @@ namespace PrimeCalc
                 return false;
             int sqrt = (int)Math.Sqrt(number);
             for (int i = 2; i <= sqrt; i++) 
-            {
                 if (number % i == 0) { return false; }
-            }
             return true;
         }
     }
